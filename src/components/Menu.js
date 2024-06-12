ad1100bcdcdbbb836f80cdf502ca3458ac162202
Victor Gou <victorgou.design@gmail.com>
2024-06-12 16:04:55 -0300

    click on menu option -> close menu.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,34 +4,33 @@ import './Menu.css';
 import { useMain } from '../states/main_state'; // Adjust the path as necessary
 import TypewriterText from '../fx/TypewriterText';
 
-
-const Menu = () => {
+const Menu = ({ toggleMenu }) => {
   const { setCurrentMain } = useMain();
-  
 
   const handleButtonClick = (newMain) => {
     setCurrentMain(newMain);
-  }
+    toggleMenu(); // Close the menu after clicking an option
+  };
 
-    return (
-      <div className="menu">
-        <a href='' className="option" >
-          <TypewriterText hover={true}>/init *</TypewriterText>
-        </a>
-        <button className="option" onClick={() => { handleButtonClick('intro_main'); }}>
-          <TypewriterText hover={true}>/intro</TypewriterText>
-        </button>
-        <button className="option" onClick={() => { handleButtonClick('alpha_main'); }}>
-          <TypewriterText hover={true}>/alpha</TypewriterText>
-        </button>
-        <button className="option" onClick={() => { handleButtonClick('beta_main'); }}>
-          <TypewriterText hover={true}>/beta</TypewriterText>
-        </button>
-        <button className="option" onClick={() => { handleButtonClick('museum_main'); }}>
-          <TypewriterText hover={true}>/museum</TypewriterText>
-        </button>
-      </div>
-    );
+  return (
+    <div className="menu">
+      <a href='' className="option">
+        <TypewriterText hover={true}>/init *</TypewriterText>
+      </a>
+      <button className="option" onClick={() => { handleButtonClick('intro_main'); }}>
+        <TypewriterText hover={true}>/intro</TypewriterText>
+      </button>
+      <button className="option" onClick={() => { handleButtonClick('alpha_main'); }}>
+        <TypewriterText hover={true}>/alpha</TypewriterText>
+      </button>
+      <button className="option" onClick={() => { handleButtonClick('beta_main'); }}>
+        <TypewriterText hover={true}>/beta</TypewriterText>
+      </button>
+      <button className="option" onClick={() => { handleButtonClick('museum_main'); }}>
+        <TypewriterText hover={true}>/museum</TypewriterText>
+      </button>
+    </div>
+  );
 };
 
 export default Menu;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,23 +6,23 @@ import Menu from './Menu';
 
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
+
     const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
-        <nav className="navbar" >
-        <div className="navbar-container">
-            <a href='' className="navbar-brand">
-                <img src="https://avatars.githubusercontent.com/u/151031767?s=64&v=4" alt="Icon" className="navbar-icon" />
-                <span className="navbar-title"><TypewriterText>SKYNET.GPU</TypewriterText></span>
-            </a>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <a href='' className="navbar-brand">
+                    <img src="https://avatars.githubusercontent.com/u/151031767?s=64&v=4" alt="Icon" className="navbar-icon" />
+                    <span className="navbar-title"><TypewriterText>SKYNET.GPU</TypewriterText></span>
+                </a>
                 <a href="#" className="navbar-link" onClick={toggleMenu}>
                     <TypewriterText hover={true} ticking={true}>/root</TypewriterText>   
                 </a>
-                {isMenuOpen && <Menu />} {/* Show Menu component when isMenuOpen is true */}
-        </div>
+                {isMenuOpen && <Menu toggleMenu={toggleMenu} />} {/* Pass toggleMenu as a prop */}
+            </div>
         </nav>
     );
 };
