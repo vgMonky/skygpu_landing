4324399f2912ac1d81b60f1cbd8ed44b87988c08
Victor Gou <victorgou.design@gmail.com>
2024-06-12 17:22:42 -0300

    menu * indicates with star

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,33 +1,36 @@
-// /src/component/menu.js
 import React from 'react';
 import './Menu.css';
 import { useMain } from '../states/main_state'; // Adjust the path as necessary
 import TypewriterText from '../fx/TypewriterText';
 
 const Menu = ({ toggleMenu }) => {
-  const { setCurrentMain } = useMain();
+  const { currentMain, setCurrentMain } = useMain();
 
   const handleButtonClick = (newMain) => {
     setCurrentMain(newMain);
     toggleMenu(); // Close the menu after clicking an option
   };
 
+  const getOptionText = (option, displayText) => {
+    return currentMain === option ? `${displayText} *` : displayText;
+  };
+
   return (
     <div className="menu">
       <a href='' className="option">
-        <TypewriterText hover={true}>/init *</TypewriterText>
+        <TypewriterText hover={true}>{getOptionText('home_main', '/init')}</TypewriterText>
       </a>
       <button className="option" onClick={() => { handleButtonClick('intro_main'); }}>
-        <TypewriterText hover={true}>/intro</TypewriterText>
+        <TypewriterText hover={true}>{getOptionText('intro_main', '/intro')}</TypewriterText>
       </button>
       <button className="option" onClick={() => { handleButtonClick('alpha_main'); }}>
-        <TypewriterText hover={true}>/alpha</TypewriterText>
+        <TypewriterText hover={true}>{getOptionText('alpha_main', '/alpha')}</TypewriterText>
       </button>
       <button className="option" onClick={() => { handleButtonClick('beta_main'); }}>
-        <TypewriterText hover={true}>/beta</TypewriterText>
+        <TypewriterText hover={true}>{getOptionText('beta_main', '/beta')}</TypewriterText>
       </button>
       <button className="option" onClick={() => { handleButtonClick('museum_main'); }}>
-        <TypewriterText hover={true}>/museum</TypewriterText>
+        <TypewriterText hover={true}>{getOptionText('museum_main', '/museum')}</TypewriterText>
       </button>
     </div>
   );
