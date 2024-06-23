f725545ef7d1621de5212730bf88649efb659f8b
Viterbo <viter.rod@gmail.com>
2024-06-23 11:59:09 -0300

    add settings switchers

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className='crt'>
       <div className='screen'></div>
+      <div className='frame'></div>
       <MainProvider>
         <ViewerStateProvider>
           <SettingsProvider>
--- src/states/settings_state.js
+++ src/states/settings_state.js
@@ -14,6 +14,16 @@ export const SettingsProvider = ({ children }) => {
     return savedMute !== null ? JSON.parse(savedMute) : false; // Default to false
   });
 
+  const [crtFxEnabled, setCrtFxEnabled] = useState(() => {
+    const savedCrtFx = localStorage.getItem('crtFxEnabled');
+    return savedCrtFx !== null ? JSON.parse(savedCrtFx) : true; // Default to true
+  });
+
+  const [frameFxEnabled, setFrameFxEnabled] = useState(() => {
+    const savedFrameFx = localStorage.getItem('frameFxEnabled');
+    return savedFrameFx !== null ? JSON.parse(savedFrameFx) : true; // Default to true
+  });
+
   useEffect(() => {
     localStorage.setItem('typeFxEnabled', JSON.stringify(typeFxEnabled));
   }, [typeFxEnabled]);
@@ -25,8 +35,35 @@ export const SettingsProvider = ({ children }) => {
     });
   }, [muteEnabled]);
 
+  useEffect(() => {
+    localStorage.setItem('crtFxEnabled', JSON.stringify(crtFxEnabled));
+    const screen = document.querySelector('.screen');
+    if (crtFxEnabled) {
+      screen.style.display = 'block';
+    } else {
+      screen.style.display = 'none';
+      setFrameFxEnabled(false);
+    }
+  }, [crtFxEnabled]);
+
+  useEffect(() => {
+    localStorage.setItem('frameFxEnabled', JSON.stringify(crtFxEnabled));
+    const frame = document.querySelector('.frame');
+    if (frameFxEnabled) {
+      setCrtFxEnabled(true);
+      frame.style.display = 'block';
+    } else {
+      frame.style.display = 'none';  
+    }
+  }, [frameFxEnabled]);
+
   return (
-    <SettingsContext.Provider value={{ typeFxEnabled, setTypeFxEnabled, muteEnabled, setMuteEnabled }}>
+    <SettingsContext.Provider value={{
+      typeFxEnabled, setTypeFxEnabled,
+      muteEnabled, setMuteEnabled,
+      crtFxEnabled, setCrtFxEnabled,
+      frameFxEnabled, setFrameFxEnabled}}
+    >
       {children}
     </SettingsContext.Provider>
   );
